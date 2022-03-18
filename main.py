d276368cc47dae5371cc509737d1c5df0f7e4b2f
startart <63877490+42hub@users.noreply.github.com>
2022-03-18 09:00:58 +0000

    MAIN

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
-print("TEST")
-print("TEST")
-print("TEST")
-print("TEST")
-print("TEST")
-print("TEST")
-print("TEST")
-print("TEST")
+from flask import Flask, request
+import random
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/getstatus')
+def getstatus():
+    return 'The service should be working.'
+
+@app.route('/main')
+def main():
+	page = request.args.get('page', default = 1, type = int)
+	filter = request.args.get('filter', default = '*', type = str)
+	return f"Test{page, filter}"
+
+
+if __name__ == "__main__":
+	app.run(
+		host='0.0.0.0',
+		port=80
+	)
